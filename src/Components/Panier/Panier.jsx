8d18e4f2a3e8c38e8c5a257fae6ddf60b20aa47e
jxstduncan <@Bergmans.duncan@gmail.com>
2025-05-14 11:24:54 +0200

    .map pour le panier fait manque que quand ont clique sur le bouton ça met un au quantité

--- src/Components/Panier/Panier.jsx
+++ src/Components/Panier/Panier.jsx
@@ -1,11 +1,16 @@
 function Panier(props){
+    console.log(props.article);
+    
     return(
         <div>
             <div>
+                <div>
+                 <h3>Panier :</h3>   
+                </div>
              {
-                props.article.map(articles=>{
-                    <p>{props.article.quantite} </p>
-                })
+                props.article.map(article =>(
+                    <p> {article.nom} {article.quantite}  </p>
+                ))
              } 
             </div>
         </div>
